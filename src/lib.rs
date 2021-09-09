1a497da507379688cd6028e21a0b3cc62008825a
vvmikheev <mikheev.vv@phystech.edu>
2021-09-09 10:37:04 +0300

    Split tests into more informative

--- src/lib.rs
+++ src/lib.rs
@@ -6,51 +6,46 @@ mod tests {
     use std::thread;
 
     #[test]
-    fn data_read_test(){
+    fn data_read(){
         #[derive(Debug, PartialEq)]
         struct Test;  // trying to send smth which is not Copy or Clone
 
         let mut mydata = Vec::with_capacity(100);
         for _i in 0..100{
             let elem = Test;
-            mydata.push(elem)
+            mydata.push(elem);
         }
 
-        let n_threads: usize = 4;
-        let mut handler = Vec::with_capacity(n_threads);
+        let mut queue = WaitFreeQueue::new(mydata);
 
-        let queue = WaitFreeQueue::new(mydata);
-
-        for _i in 0..n_threads{
-
-            let mut thread_queue = queue.clone();
-
-            handler.push(
-            thread::spawn(move || {
-                loop{
-                    let elem = thread_queue.get_next_mut();
-                    if elem.is_some(){
-                        assert_eq!(*elem.unwrap(), Test);
-                    }
-                    else {
-                        break
-                    }
-                }
-
-            }))
+        loop{
+            let elem = queue.get_next_mut();
+            match elem{
+                Some(data) => assert_eq!(*data, Test),
+                None => break
+            }
         }
-
-        for handle in handler {
-            match handle.join(){
-                Ok(_) => {},
-                Err(e) => {panic!(e)}
-            };
-        }
-
     }
 
     #[test]
-    fn data_write_test() {
+    fn write_test(){
+        let mydata = vec![1; 100];
+        let mut queue = WaitFreeQueue::new(mydata);
+
+        loop{
+            let elem = queue.get_next_mut();
+            match elem {
+                Some(data) => {*data += 1},
+                None => break
+            }
+        }
+
+        let new_data = queue.get_data();
+        assert_eq!(new_data, vec![2; 100].as_slice());
+    }
+
+    #[test]
+    fn parallel_write_test() {
         let mydata = vec![1; 100];
         let n_threads: usize = 4;
         let mut handler = Vec::with_capacity(n_threads);
